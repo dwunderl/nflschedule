cbe08e82a72b3bcd6e4e7ae00dc3d156f7c307bd
dwunderl <dwunderl@mac-mini.local>
2019-02-02 19:12:38 -0700

    NflScheduleAlert - added, implemented to allow alert objects to be collected and reported in schedule spreadsheet

---
+++ NflSchedule/src/com/nflscheduling/NflScheduleAlert.java
@@ -0,0 +1,14 @@
+package com.nflscheduling;
+
+public class NflScheduleAlert {
+	
+	public String alertDescr;
+	public int weekNum;
+	public String homeTeam;
+	public String awayTeam;
+
+	public NflScheduleAlert() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
